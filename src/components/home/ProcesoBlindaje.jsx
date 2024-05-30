ac67d9e96d3672471558573c51960a6688fe8fd3
Pablo Lires <pablolires@gmail.com>
2024-05-30 15:21:33 -0300

    cambios de icono y textos

--- src/components/home/ProcesoBlindaje.jsx
+++ src/components/home/ProcesoBlindaje.jsx
@@ -1,5 +1,5 @@
 import styles from './proceso-blindaje.module.css'
-import CardProceso from '@/components/home/CardProceso'
+// import CardProceso from '@/components/home/CardProceso'
 import { getProcesoBlindaje } from '@/utils/dataUtils.js'
 
 const ProcesoBlindaje = () => {
@@ -14,14 +14,27 @@ const ProcesoBlindaje = () => {
         </div>
       </div>
       <div className='row'>
-        {procesosBlindaje.map(item => (
+        <div className='col-md-12'>
+          <p>
+            En Elbin trabajamos constantemente en ayudar a nuestros/as clientes
+            a planificar de manera sana diferentes situaciones financieras. Es
+            por esto que nuestro proceso de blindaje es tan importante para
+            nosotros/as. Por medio de este método nos aseguramos de que las
+            personas que confían en el asesoramiento que brindamos, puedan
+            conservar a lo largo de sus vidas, las de sus familias y empresas,
+            todo lo construido hasta el momento. La mayor motivación que tenemos
+            es blindar sus patrimonios, objetivos y sueños para que pase lo que
+            pase, nada modifique lo planeado.
+          </p>
+        </div>
+        {/* {procesosBlindaje.map(item => (
           <CardProceso
             key={item.id}
             data-aos='fade-up'
             title={item.title}
             description={item.description}
           />
-        ))}
+        ))} */}
       </div>
     </section>
   )
